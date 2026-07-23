dce24ab9a41a921eb39cf7d07606d08349e01d76
dionnell <dionnellr1@gmail.com>
2026-07-23 19:54:34 -0400

    feat: enhance ValidationPipe configuration with implicit transformation options

--- src/main.ts
+++ src/main.ts
@@ -26,6 +26,10 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
     })
   );
 
